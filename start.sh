4dc6c967ce78c2cc07a81a2b89892cd548c75f2c
Drylian <109999325+drylian@users.noreply.github.com>
2022-11-30 13:11:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,25 @@
 #!/bin/ash
+
+echo "Deletando Arquivos Temporários"
+
 rm -rf /home/container/tmp/*
 
-echo "⟳ Iniciando PHP-FPM..."
-/usr/sbin/#!/bin/ash
-rm -rf /home/container/tmp/*
+echo "Arquivos temporários deletados"
+
+cd /home/container
 
 echo "⟳ Iniciando PHP-FPM..."
-/usr/sbin/php-fpm81 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+php --version
+/usr/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+
+echo "✓ Iniciado o PHP-FPM"
 
 echo "⟳ Iniciando Nginx..."
+
+/usr/sbin/nginx -v
+
 echo "✓ Inicializado com sucesso"
+
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+
+echo "✓ Inicializado com sucesso"
